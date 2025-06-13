ef83e01bc5840f53176e1b92ae4b0403849b6974
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-06-13 19:02:16 -0300

    Listar milhares de usuários

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,10 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function index() {
-    $user = User::first();  
-    return view("admin.users.index", compact('user'));
+
+    $users = User::paginate(15); //User::all();  
+    
+    return view("admin.users.index", compact('users'));
+    
     }
 }
